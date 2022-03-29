ababf50ee72a34092b4eb2bf87414b3a6c7c901a
Joshua <ohisologbosere@gmail.com>
2022-03-29 18:09:02 +0100

    initial commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,13 +2,13 @@ import NextAuth from "next-auth"
 import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
-  // Configure one or more authentication providers
+  
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
-    // ...add more providers here
+    
   ],
-  secret: process.env.JWT_SECRET,
+  
 })
